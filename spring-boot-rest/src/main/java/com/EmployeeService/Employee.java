cb10a34ab77fb1c60b398d4f2796a666239ef76f
kgudd <kgudd@192.168.0.19>
2018-10-04 21:33:41 -0500

    test

--- spring-boot-rest/src/main/java/com/EmployeeService/Employee.java
+++ spring-boot-rest/src/main/java/com/EmployeeService/Employee.java
@@ -1,49 +1,7 @@
 package com.EmployeeService;
 
 public class Employee {
-	Long id;
-	String firstName;
-	String lastName;
-	Integer salary;
-	public Employee() {
-		
-	}
-	public Employee(Long id, String firstName, String lastName, Integer salary) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.salary = salary;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public Integer getSalary() {
-		return salary;
-	}
-	public void setSalary(Integer salary) {
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
-				+ "]";
-	}
-	
+
 	
 	
 }
